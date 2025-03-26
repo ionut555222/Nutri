b5a8e30480ade034ed03db33d3fe2dee9d530496
Popescu Ioan-Andrei <popescuioanandrei@yahoo.ro>
2025-03-26 12:28:44 +0200

    Initial Commit

---
+++ project 2Tests/project_2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  project_2Tests.swift
+//  project 2Tests
+//
+//  Created by Ionut Popescu on 26.03.2025.
+//
+
+import Testing
+@testable import project_2
+
+struct project_2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
